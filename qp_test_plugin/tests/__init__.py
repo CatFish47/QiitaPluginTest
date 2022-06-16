b59294519a01db692f24a6604a4b3a199135e3b3
Grant Cheng <chengxgrant@gmail.com>
2022-06-16 14:38:47 -0700

    Test qiita plugin

---
+++ qp_test_plugin/tests/__init__.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022, Not a Team.
+#
+# Distributed under the terms of the MIT License License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
